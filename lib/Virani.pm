1a2336713a23abba924d54d835cf58e6a0c4a997
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2023-07-31 12:00:54 -0500

    ready for release

--- lib/Virani.pm
+++ lib/Virani.pm
@@ -21,11 +21,11 @@ Virani - PCAP retrieval for a FPC setup writing to PCAP files.
 
 =head1 VERSION
 
-Version 0.1.1
+Version 0.1.0
 
 =cut
 
-our $VERSION = '0.1.1';
+our $VERSION = '0.1.0';
 
 =head1 SYNOPSIS
 
