6a7c47449bf7195682a75c82b1dd57eda97b337d
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2023-08-07 11:28:27 -0500

    bump to 1.0.1 and note the fixes

--- lib/Virani.pm
+++ lib/Virani.pm
@@ -22,11 +22,11 @@ Virani - PCAP retrieval for a FPC setup writing to PCAP files.
 
 =head1 VERSION
 
-Version 1.0.0
+Version 1.0.1
 
 =cut
 
-our $VERSION = '1.0.0';
+our $VERSION = '1.0.1';
 
 =head1 SYNOPSIS
 
